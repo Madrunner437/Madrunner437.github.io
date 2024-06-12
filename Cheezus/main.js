d9aaaee41fa1559edf18f25cab187fd8e9b116a1
Madrunner437 <83588880+Madrunner437@users.noreply.github.com>
2024-06-12 19:12:14 +0100

    Update main.js

--- Cheezus/main.js
+++ Cheezus/main.js
@@ -4,7 +4,10 @@ import * as THREE from "./three/build/three.module.js";
 // Three.js Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
+// Add a large sphere to the scene
 const sphereGeometry = new THREE.SphereGeometry(500, 32, 32); // Increase the radius to make it large
 const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // White color
 const backgroundSphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
