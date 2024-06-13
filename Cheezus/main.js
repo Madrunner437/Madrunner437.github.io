8ad6781a8ae797c8c0291322b7c14d980699b86e
Madrunner437 <83588880+Madrunner437@users.noreply.github.com>
2024-06-13 11:57:56 +0100

    Update main.js

--- Cheezus/main.js
+++ Cheezus/main.js
@@ -40,7 +40,6 @@ document.getElementById('start-button').addEventListener('click', function() {
 function animate() {
     requestAnimationFrame(animate);
     backgroundSphere.rotation.y += 0.001; // Rotate the sphere for effect
-    controls.update()
     renderer.render(scene, camera);
 }
 
